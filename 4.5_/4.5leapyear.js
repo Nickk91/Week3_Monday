18e06c0c3d127760fa25f198632e46451f550fd8
Nickk91 <143394682+Nickk91@users.noreply.github.com>
2023-10-15 13:27:47 +0300

    Add files via upload

--- 4.5_/4.5leapyear.js
+++ 4.5_/4.5leapyear.js
@@ -1,9 +1,18 @@
-year=2000;
-
-if (year %4 === 0){
-    if (year %100 ===0){
-        if year %400==0{
-            
-        }
+function check(year) {
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) {
+        console.log("It is indeed a leap year");
+      } else {
+        console.log("This is not a leap year.");
+      }
+    } else {
+      console.log("It is indeed a leap year");
     }
+  } else {
+    console.log("This is not a leap year.");
+  }
 }
+// check(2012);
+// check(2100);
+// check(2400);
